8e8120da78bb2372f53a3170030d932729a70318
Keval M Jain <keval1998.work@outlook.com>
2020-07-27 14:27:40 +0530

    First Program

---
+++ JavaTraining/src/Factorial.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Factorial {
+
+	public static void main(String[] args) {
+		caluclateFact();
+		
+		
+		}
+	public static void caluclateFact() {
+		int number,factorial=1;
+		Scanner s= new Scanner(System.in);
+		System.out.println("Enter a number");
+		number= s.nextInt();
+		for(int i=1; i<= number; i++) {
+			factorial= factorial*i;
+		}
+		System.out.println("Factorial of "+number+" is "+ factorial );
+		
+	}
+
+}
